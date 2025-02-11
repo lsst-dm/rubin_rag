4a263ebad83de10515e1878f1df3f500b39abeaf
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-02-11 15:38:33 -0700

    try consdb-like __init__.py

--- python/rubin/rag/__init__.py
+++ python/rubin/rag/__init__.py
@@ -1,16 +1,22 @@
-"""Rubin RAG-basd LLM Application."""
+# This file is part of rubin_rag.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ["__version__"]
-
-from importlib.metadata import PackageNotFoundError, version
-
-__version__: str
-"""The version string of rubin.rag
-(PEP 440 / SemVer compatible).
-"""
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "0.0.1"
+# from .version import *
