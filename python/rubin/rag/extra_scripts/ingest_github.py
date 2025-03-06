bea6973c2e6bdadfabe38a6869eb1ad13db3da99
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-03-06 12:52:18 -0700

    logging

--- python/rubin/rag/extra_scripts/ingest_github.py
+++ python/rubin/rag/extra_scripts/ingest_github.py
@@ -6,6 +6,7 @@
 # see https://python.langchain.com/docs/integrations/document_loaders/github/
 
 import requests
+import logging
 from langchain.schema.document import Document
 from langchain_community.document_loaders import GithubFileLoader
 
