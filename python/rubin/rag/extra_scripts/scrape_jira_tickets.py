b5c032b57c711903fe0e0c9fa3fbc0f546983c5e
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-03-28 19:40:32 -0700

    linting

--- python/rubin/rag/extra_scripts/scrape_jira_tickets.py
+++ python/rubin/rag/extra_scripts/scrape_jira_tickets.py
@@ -447,6 +447,4 @@ def jira_to_document(jira_data: dict) -> Document:
     metadata = jira_data.copy()
     del metadata["description"]
 
-    return Document(
-        page_content=jira_data["description"], metadata=metadata
-    )
+    return Document(page_content=jira_data["description"], metadata=metadata)
