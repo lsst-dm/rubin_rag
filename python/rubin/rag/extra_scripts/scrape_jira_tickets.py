125dc37a0985da6f5b6780264a3bd2d125b5f9a3
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-03-28 18:18:03 -0700

    start moving API credentials to env variables

--- python/rubin/rag/extra_scripts/scrape_jira_tickets.py
+++ python/rubin/rag/extra_scripts/scrape_jira_tickets.py
@@ -29,11 +29,15 @@ import time
 from functools import reduce
 from pathlib import Path
 from typing import Any
+import os
 
 import requests
 
-
-def get_jira_issue(issue_name: str, email: str, api_token: str) -> tuple:
+def get_jira_issue(
+    issue_name: str,
+    email: str,
+    api_token: str = os.getenv("ATLASSIAN_API_TOKEN")
+) -> tuple:
     """Get the JIRA issue data from the JIRA API.
 
     Parameters
