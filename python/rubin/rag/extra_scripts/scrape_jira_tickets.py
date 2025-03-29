c519aa7c999e27fa6fe838ab4a4ec030919aef68
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-03-29 02:21:59 -0700

    linting

--- python/rubin/rag/extra_scripts/scrape_jira_tickets.py
+++ python/rubin/rag/extra_scripts/scrape_jira_tickets.py
@@ -554,4 +554,5 @@ def jira_tickets_in_range(
         for i in range(min_ticket_num, max_ticket_num + 1)
     ]
     return jira_tickets_from_list(
-        ticket_list, email, api_token, folder=folder, write=write)
+        ticket_list, email, api_token, folder=folder, write=write
+    )
