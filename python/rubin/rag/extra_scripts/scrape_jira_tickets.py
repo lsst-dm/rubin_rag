163594495924d9ba6bb84b6a48df0a7d88ab6f9f
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-04-01 11:22:48 -0700

    use retry functionality when looping over multiple tickets

--- python/rubin/rag/extra_scripts/scrape_jira_tickets.py
+++ python/rubin/rag/extra_scripts/scrape_jira_tickets.py
@@ -508,7 +508,7 @@ def jira_tickets_from_list(
     docs: list = []
 
     for ticket_name in ticket_list:
-        jira_data, status = fetch_ticket(ticket_name, email, api_token)
+        jira_data, status = retry_fetch_ticket(ticket_name, email, api_token)
         # only output the results if fetching was successful
         if status is None:
             docs.append(jira_to_document(jira_data))
