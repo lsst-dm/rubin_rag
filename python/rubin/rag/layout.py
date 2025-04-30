0d98b91b53d9699cc2325bf4a0b8e3a77b50f226
cyab05 <cyab05@uw.edu>
2025-04-30 14:51:04 -0700

    Linting

--- python/rubin/rag/layout.py
+++ python/rubin/rag/layout.py
@@ -32,9 +32,7 @@ from langchain_community.chat_message_histories import (
 
 def setup_sidebar() -> None:
     """Set up the sidebar for the Streamlit app."""
-    select_sources_text = (
-        "Select sources to search from."
-    )
+    select_sources_text = "Select sources to search from."
     st.sidebar.markdown(select_sources_text)
     st.session_state["required_sources"] = []
     if st.sidebar.checkbox("Confluence", value=True):
