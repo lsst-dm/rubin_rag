23d82d5a556686bed5ec9492eaafcc19625c4b6d
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-05-12 21:25:02 -0700

    linting

--- python/rubin/rag/extra_scripts/scrape_github_fast.py
+++ python/rubin/rag/extra_scripts/scrape_github_fast.py
@@ -77,7 +77,7 @@ def scrape_1org(org_name='lsst-dmsst', write=False):
 
     if write:
         with open(org_name + "_20250502.pickle", "wb") as handle:
-            pickle.dump(all_docs, handle, protocol=pickle.HIGHEST_PROTOCOL)       
+            pickle.dump(all_docs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     return all_docs
 
