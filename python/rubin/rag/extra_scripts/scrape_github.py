46c0576f8f547735ae44dc448342c79fbf067b3c
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-05-18 19:40:00 -0700

    ignore .out files; LSSTDESC has some very large .out files

--- python/rubin/rag/extra_scripts/scrape_github.py
+++ python/rubin/rag/extra_scripts/scrape_github.py
@@ -143,6 +143,7 @@ def clean_file_list(directory: str = "rubin_rag") -> list[str]:
         and (f[-4:] != ".eps")
         and (f[-4:] != ".tar")
         and (f[-4:] != ".zip")
+        and (f[-4:] != ".out")
         and ("gen2" not in f.lower())
         and ("data" not in os.path.split(f)[0].lower())
         and ("images" not in f.split("/"))
