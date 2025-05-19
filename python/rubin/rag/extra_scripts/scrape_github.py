cda1c196d4368f0b8435c2ab33c86483c5407e84
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-05-18 23:25:01 -0700

    skip .SIMLIB files (see e.g., sn_lc2cosmo_tutorials in LSSTDESC)

--- python/rubin/rag/extra_scripts/scrape_github.py
+++ python/rubin/rag/extra_scripts/scrape_github.py
@@ -162,6 +162,7 @@ def clean_file_list(directory: str = "rubin_rag") -> list[str]:
         and (f[-4:] != ".tar")
         and (f[-4:] != ".zip")
         and (f[-4:] != ".out")
+        and (f[-7:] != ".SIMLIB")
         and ("gen2" not in f.lower())
         and ("data" not in os.path.split(f)[0].lower())
         and ("images" not in f.split("/"))
