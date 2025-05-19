2b413b784c618ec0e2b30f30b4ad47eaa6e92141
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-05-18 18:22:42 -0700

    ignore images/ and figures/ directories

--- python/rubin/rag/extra_scripts/scrape_github.py
+++ python/rubin/rag/extra_scripts/scrape_github.py
@@ -114,6 +114,8 @@ def clean_file_list(directory: str = "rubin_rag") -> list[str]:
         and (f[-4:] != ".eps")
         and ("gen2" not in f.lower())
         and ("data" not in os.path.split(f)[0].lower())
+        and ("images" not in f.split("/"))
+        and ("figures" not in f.split("/"))
     ]
 
 
