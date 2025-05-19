08767f8e7052dc920acb4834036ce732079c281b
Aaron Meisner <aaron.meisner@noirlab.edu>
2025-05-19 10:30:03 -0700

    skip .lvproj files

--- python/rubin/rag/extra_scripts/scrape_github.py
+++ python/rubin/rag/extra_scripts/scrape_github.py
@@ -172,6 +172,7 @@ def clean_file_list(directory: str = "rubin_rag") -> list[str]:
         and (f[-3:] != ".pd")
         and (f[-7:] != ".SIMLIB")
         and (f[-7:] != ".pickle")
+        and (f[-7:] != ".lvproj")
         and ("gen2" not in f.lower())
         and ("data" not in os.path.split(f)[0].lower())
         and ("images" not in f.split("/"))
